fb4546f388627e7c0ba1259961911f9a8ad3f4a4
Will Burke <billyburke1012@gmail.com>
2021-01-26 16:01:55 -0500

    wizard forms commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+
+import WizardForm from './compnents/WizardForm'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <WizardForm 
+      title="Add New Wizard"
+      onSubmit={(wizard) =>{
+        console.log('__________________________')
+        console.log('here is the new wizard')
+        console.log(wizard)
+      }} />
     </div>
   );
 }
---
+++ src/compnents/WizardForm.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import { useState } from 'react'
+
+function WizardForm(props) {
+    const [name, setName] = useState('');
+    const [occupation, setOccupation] = useState('');
+    const [house, setHouse] = useState('');
+
+
+    return(
+        <section >
+            <h1>{props.title}</h1>
+            <form onSubmit={(e) => {
+                e.preventDefault();
+                const wizardObject ={
+                    name: name,
+                    occupation: occupation,
+                    house: house
+                }
+                props.onSubmit(wizardObject)
+
+            }}> 
+                <label>Name:
+                    <input 
+                        placeholder="enter name"
+                        value={name}
+                        onChange={(e) => {
+                            setName(e.target.value);
+                        }}
+                    />
+                    </label>
+                <br></br>
+                <label>Occupation:
+                        <input
+                            placeholder="enter occupation"
+                            value={occupation}
+                            onChange={(e) => {
+                                setOccupation(e.target.value);
+                            }}
+                        />
+                </label>
+                <br/>
+                <label>House:
+                        <input
+                            placeholder="enter house"
+                            value={house}
+                            onChange={(e) => {
+                                setHouse(e.target.value);
+                            }}
+                        />
+                </label>
+                <br/>
+                <input type="submit"/>
+            </form>
+        </section>
+    )
+}
+
+
+export default WizardForm;
